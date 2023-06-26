7f47efa17481c7240e009e0dd765ea192ddb2c1c
djr76 <53257673+djr76@users.noreply.github.com>
2023-06-26 10:59:40 -0400

    added sum-range and whats in common

--- problems/general/whats-in-common/index.js
+++ problems/general/whats-in-common/index.js
@@ -6,8 +6,20 @@
  * @return {number|string[]}
 */
 
-function whatsInCommon(array1, array2) {
+// Wasn't able to figure this one out
 
+function whatsInCommon(array1, array2) {
+  let matches = [];
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        matches.push(array1[i])
+      }
+    }
+  }
+  return matches
 }
+
+whatsInCommon([1, 2, 3], [2, 4, 3,]);
 
 //module.exports = whatsInCommon;
